e0b1409058f5339d3dded6e8c516a39c62326e9e
s4iTen <ychouk102@gmail.com>
2023-02-21 15:54:30 +0100

    this is the file of the unitest

---
+++ tests/base_model_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Doc
+"""
+from models.base_model import *
+from models.base_model import BaseModel
+
+
+class BaseModel(BaseModel):
+    """Doc
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Doc
+        """
+        self.created_at = "Fake"
