2a0672805b1163dd0b7e460a98c82f73b779c9b1
s4iTen <ychouk102@gmail.com>
2023-02-21 18:32:49 +0100

    this function check the date from the dictionary and set it to the public instance variables that we make or if its already exist create anew id and new date of creation

--- models/base_model.py
+++ models/base_model.py
@@ -3,19 +3,30 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """this is the main Module"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+            
+            self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            
     def __str__(self):
         """this Method change the print function"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """this Method return a dictionary containing all key/value
