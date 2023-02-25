8351e31f9fc6afc8b9d1e8e4b5ac97b78910e1e5
s4iTen <ychouk102@gmail.com>
2023-02-25 01:02:10 +0100

    this is the module place

---
+++ models/place.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is the Place class"""
+from models.base_model import BaseModel
+from models.city import City
+from models.user import User
+from models.amenity import Amenity
+
+
+class Place(BaseModel):
+    city_id = City.id
+    user_id = User.id
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = Amenity.id
